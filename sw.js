54245d126c417d9cefddea768ec965ed81b7f675
ardila92i <lukasteso@gmail.com>
2025-11-28 14:51:31 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'radiz-portal-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/logoradiz.png',
+  '/radiz-driver.jpg.jpg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE).catch(err => {
+        console.log('Error cacheando recursos iniciales', err);
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
